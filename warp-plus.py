2fb091eff8db8119187c167920294a60036fdaf6
wgp-2020 <70625361+wgp-2020@users.noreply.github.com>
2022-09-01 19:40:26 +0800

    Add files via upload

--- warp-plus.py
+++ warp-plus.py
@@ -52,8 +52,8 @@ def run():
             sys.exit(0)
 
 ts = 23
-e_rt = os.getenv('RUN_TIME')
-runTime = e_rt if e_rt else 14100
+e_rt = int(os.getenv('RUN_TIME'))
+runTime = e_rt*60 if e_rt else 14100
 suc = 0
 bad = 0
 while True:
