29a5b36d76cfcf7db4c2047c3aeb20810fe9541b
wgp-2020 <70625361+wgp-2020@users.noreply.github.com>
2022-09-01 14:53:55 +0800

    Add files via upload

--- warp-plus.py
+++ warp-plus.py
@@ -49,71 +49,9 @@ def run():
 ts = 23
 num = 0
 while True:
-	import urllib.request
-import json
-import datetime
-import random
-import string
-import time
-import os
-
-print ("WARP-PLUS-CLOUDFLARE By ALIILAPRO")
-referrer = os.getenv('DEVICE_ID')
-def genString(stringLength):
-	try:
-		letters = string.ascii_letters + string.digits
-		return ''.join(random.choice(letters) for i in range(stringLength))
-	except Exception as error:
-		print(error)		    
-def digitString(stringLength):
-	try:
-		digit = string.digits
-		return ''.join((random.choice(digit) for i in range(stringLength)))    
-	except Exception as error:
-		print(error)	
-url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
-def run():
-	try:
-		install_id = genString(22)
-		body = {"key": "{}=".format(genString(43)),
-				"install_id": install_id,
-				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-				"referrer": referrer,
-				"warp_enabled": False,
-				"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
-				"type": "Android",
-				"locale": "es_ES"}
-		data = json.dumps(body).encode('utf8')
-		headers = {'Content-Type': 'application/json; charset=UTF-8',
-					'Host': 'api.cloudflareclient.com',
-					'Connection': 'Keep-Alive',
-					'Accept-Encoding': 'gzip',
-					'User-Agent': 'okhttp/3.12.1'
-					}
-		req         = urllib.request.Request(url, data, headers)
-		response    = urllib.request.urlopen(req)
-		status_code = response.getcode()	
-		return status_code
-	except Exception as error:
-		print(error)	
-
-ts = 23
-num = 0
-while True:
-	if num >= 2: break
 	result = run()
 	if result == 200:
-		num = num + 1
-		print(f"成功！总计获取{num}GB流量")
-		if ts > 23: ts = ts - 1
-		time.sleep(ts)
-	else:
-		ts = ts + 1
-		print(f"失败！将等待{ts}秒")	
-		time.sleep(ts)
-	result = run()
-	if result == 200:
-		num = num + 1
+		num = num +1
 		print(f"成功！总计获取{num}GB流量")
 		if ts > 23: ts = ts - 1
 		time.sleep(ts)
